f6bed65192003f55724d316720a192d411018f59
Chirag_Chhajlani <Chirag.Chhajlani@Dell.com>
2022-05-12 23:55:33 +0530

    Included initial course files

---
+++ Starter Code/complete-javascript-course/01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,28 @@
+// LECTURE : Values and Variables
+// Task 1
+const COUNTRY = "India";
+const CONTINENT = "Asia";
+let population = 1381;
+
+// Task 2
+console.log("Country : " + COUNTRY + "\nContinent : " + CONTINENT + "\nPopulation : " + population + " (in millions)");
+
+// LECTURE : Data Types
+// Task 1
+const isISLAND = false;
+let language;
+
+//Task 2
+console.log("isIsland Data Type : " + typeof isISLAND);
+console.log("population Data Type : " + typeof population);
+console.log("country Data Type : " + typeof COUNTRY);
+console.log("language Data Type : " + typeof language);
+
+//LECTURE : let, const, and var
+//Task 1
+language = "Hindi";
+
+//Task 3 : Get an error as constant assignment cannot be altered
+//COUNTRY = "SINGAPORE";
+
+console.log("language Data Type : " + typeof language);
