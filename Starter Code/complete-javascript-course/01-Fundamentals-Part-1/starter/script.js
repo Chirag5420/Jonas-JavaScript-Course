dda5db507cacec975d1c717138793415ce0773ae
Chirag Chhajlani <chiragchhajlani3@gmail.com>
2022-05-13 01:20:50 +0530

    Data Types and typeof operator

--- Starter Code/complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
+++ Starter Code/complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -1,9 +1,11 @@
-let js = "amazing";
+// -------- VALUES AND VARIABLES ----------
 /*
+let js = "amazing";
+
 if (js === "amazing") {
     alert("JavaScript is FUN!");
 }
-*/
+
 
 console.log(40 + 8 + 23 - 10);
 console.log("Jonas Schmedtmann");
@@ -29,3 +31,30 @@ let job1 = "programmer";
 let job2 = "teacher";
 
 console.log(myFirstJob);
+*/
+
+// ---------- DATA TYPES ----------- 
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//typeof operator
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 3);
+// console.log(typeof "Jonas");
+
+//dynamic typing 
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+//'undefined' means a value taken by a variable that is not yet defined. So basically it means empty value
+let year; 
+console.log(year); //OUTPUT : undefined (value)
+console.log(typeof year); //OUTPUT : undefined (type)
+
+year = 2022;
+console.log(typeof year); //OUTPUT : number
+
+//error in typeof operator
+console.log(typeof null); //null is like another data type, OUTPUT : object (regarded as a bug)
+
