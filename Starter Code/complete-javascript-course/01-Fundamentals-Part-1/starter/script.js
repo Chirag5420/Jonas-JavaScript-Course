be4dc7b6c8a9e74183c2f70a845192768a6881c3
Chirag_Chhajlani <Chirag.Chhajlani@Dell.com>
2022-05-13 00:36:30 +0530

    Values and Variables Lecture

--- Starter Code/complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
+++ Starter Code/complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -12,4 +12,20 @@ console.log(23);
 let firstName = "Jonas";
 console.log(firstName);
 
+//Variable Naming Conventions 
 
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "Jonas"; //start variable name with lowercase
+let PI = 3.1415; //always write constants in uppercase 
+
+//write descriptive variable names 
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+//instead of writing variable names like below
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
