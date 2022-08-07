b21336ae52f6288c724fb1a083a4e0d86465b57c
Chirag Chhajlani <chiragchhajlani3@gmail.com>
2022-08-07 17:16:54 +0530

    Selecting elements using querySelector()

--- Starter Code/complete-javascript-course/05-Guess-My-Number/starter/script.js
+++ Starter Code/complete-javascript-course/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// For selecting an element in JavaScript, we need to use the document.querySelector() function
+// In this method, we need to pass in a selector in the same format as we would use in CSS. 
+// As 'message' is a class, we write .message. Had it been an id, we would write #message.
+
+// We would get the paragraph (p) element with class 'message' selected and output in the console
+console.log(document.querySelector('.message')); 
+
+// If we only want to fetch the data inside this p element, we need to add .textContent() function
+console.log(document.querySelector('.message').textContent); 
