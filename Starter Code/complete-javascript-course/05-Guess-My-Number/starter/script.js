f0616806d80e060fde287be304b5c53295ecb5ae
Chirag Chhajlani <chiragchhajlani3@gmail.com>
2022-08-08 16:43:43 +0530

    Selecting and Manipulating Elements

--- Starter Code/complete-javascript-course/05-Guess-My-Number/starter/script.js
+++ Starter Code/complete-javascript-course/05-Guess-My-Number/starter/script.js
@@ -9,3 +9,17 @@ console.log(document.querySelector('.message'));
 
 // If we only want to fetch the data inside this p element, we need to add .textContent() function
 console.log(document.querySelector('.message').textContent); 
+
+// What is DOM?
+// Document Object Model : Structured Representation of HTML documents. Allows Javascript to access HTML elements and styles to manipulate them (change text, HTML attributes and even CSS styles).
+
+// DOM Methods and properties are NOT a part of JavaScript. They are a part of Web APIs. So Web APIs are libraries that browsers implements and that we can access from our JavaScript code. 
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+console.log(document.querySelector('.guess').value); // OUTPUT : 
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value); // OUTPUT : 23
