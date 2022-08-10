d3ed688827feddb6bda04912726761176c2277b9
Chirag Chhajlani <chiragchhajlani3@gmail.com>
2022-08-10 17:47:36 +0530

    Handling an "Esc" Keypress Event

--- Starter Code/complete-javascript-course/06-Modal/starter/script.js
+++ Starter Code/complete-javascript-course/06-Modal/starter/script.js
@@ -8,7 +8,6 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-    console.log('Button clicked');
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 }
@@ -24,3 +23,23 @@ for(let i = 0; i < btnsShowModal.length; i++){
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+// Keyboard events are called global events because they do not happen on one specific element. So no matter where they happen on the page, they will always trigger the event handler that we are specifying below ('keydown' for example). 
+
+// Three types of events for the keyboard :
+// - keydown --> is fired as soon as we just press down the key 
+// - keypress --> is fired continuously as we keep our finger on a certain key 
+// - keyup  --> this one only happens when we lift our finger off the keyboard or off the key
+
+// The function would be executed for any key being pressed, unless we add additional validations using the Event object e.
+// Information about which key was pressed is stored in the event that is going to occur as soon as any key is pressed, so a key down event is generated. 
+// So whenever a key down event occurs, JavaScript passes in the event object (e) as an argument
+document.addEventListener('keydown', function(e){
+    //console.log('A key was pressed!');
+    console.log(e.key);
+
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+        //console.log('Esc was pressed');
+        closeModal();
+    }
+});
