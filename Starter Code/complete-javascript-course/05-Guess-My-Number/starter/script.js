643df09c37bd85ee1663786917eeeec27cefe640
Chirag Chhajlani <chiragchhajlani3@gmail.com>
2022-08-10 06:31:01 +0530

    Manipulating CSS Styles

--- Starter Code/complete-javascript-course/05-Guess-My-Number/starter/script.js
+++ Starter Code/complete-javascript-course/05-Guess-My-Number/starter/script.js
@@ -36,10 +36,20 @@ document.querySelector('.check').addEventListener
     const guess = Number(document.querySelector('.guess').value); // using Number() function because by default when we input anything its treated as as string
     console.log(guess, typeof guess);
 
+    //When there is no input
     if(!guess){
         document.querySelector('.message').textContent = '⛔️ No number!';
+
+        //When player wins
     } else if (guess === secretNumber){
         document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+        // NOTE : Whenever we want to modify CSS styles, we always need to assign the values in a string format. And remember to write the style is camelCase notation for style names consisting of more than one word
+        document.querySelector('body').style.backgroundColor = '#60b347';
+
+        document.querySelector('.number').style.width = '30rem';
+
+        //When guess is too high
     } else if (guess > secretNumber){
         if(score > 1){
             document.querySelector('.message').textContent = '📈 Too high!';
@@ -49,6 +59,8 @@ document.querySelector('.check').addEventListener
             document.querySelector('.message').textContent = '💥 You lost the game!';
             document.querySelector('.score').textContent = 0;
         }
+
+        //When guess is too low
     } else if (guess < secretNumber){
         if(score > 1){
             document.querySelector('.message').textContent = '📉 Too low!';
