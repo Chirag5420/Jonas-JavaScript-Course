400d07d0ec452c2aa53d045edb2e7d7e79b41b74
Chirag Chhajlani <chiragchhajlani3@gmail.com>
2022-08-10 16:50:42 +0530

    Working with Classes
    Understood the usage of .classList.add() and .classList.remove() functions

--- Starter Code/complete-javascript-course/06-Modal/starter/script.js
+++ Starter Code/complete-javascript-course/06-Modal/starter/script.js
@@ -6,7 +6,21 @@ const btnCloseModal = document.querySelector('.close-modal');
 
 // Whenever we use querySelector () with a selector, which actually matches multiple elements, only the first one will get selected. So we make use of querySelectorAll() method
 const btnsShowModal = document.querySelectorAll('.show-modal');
-console.log(btnsShowModal);
 
-for(let i = 0; i < btnsShowModal.length; i++)
-    console.log(btnsShowModal[i].textContent);
+const openModal = function () {
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function (){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for(let i = 0; i < btnsShowModal.length; i++){
+    btnsShowModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
