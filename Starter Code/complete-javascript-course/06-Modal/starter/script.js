9fb845a9608f7da181e23847370de5ed7c6bf362
Chirag Chhajlani <chiragchhajlani3@gmail.com>
2022-08-10 16:27:01 +0530

    PROJECT #2: Modal Window

--- Starter Code/complete-javascript-course/06-Modal/starter/script.js
+++ Starter Code/complete-javascript-course/06-Modal/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+// Whenever we use querySelector () with a selector, which actually matches multiple elements, only the first one will get selected. So we make use of querySelectorAll() method
+const btnsShowModal = document.querySelectorAll('.show-modal');
+console.log(btnsShowModal);
+
+for(let i = 0; i < btnsShowModal.length; i++)
+    console.log(btnsShowModal[i].textContent);
