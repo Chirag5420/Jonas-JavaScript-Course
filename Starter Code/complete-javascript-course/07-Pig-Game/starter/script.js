6737cb2dd1290e4d8f08211ddca0e7e1eb457600
Chirag Chhajlani <chiragchhajlani3@gmail.com>
2022-08-20 21:33:03 +0530

    PROJECT #3: Pig Game
    Setting the initial score to 0 and hiding the dice image by adding the \'hidden\' class to the classList

--- Starter Code/complete-javascript-course/07-Pig-Game/starter/script.js
+++ Starter Code/complete-javascript-course/07-Pig-Game/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Selecting elements 
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// Setting the score to 0 
+score0El.textContent = 0;
+score1El.textContent = 0;
+
+// Disappearing the class by including the 'hidden' class 
+diceEl.classList.add('hidden');
