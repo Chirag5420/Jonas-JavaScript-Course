4ce07bb0cb34a6c8b1d8cc209253604f472e9b8f
Chirag Chhajlani <chiragchhajlani3@gmail.com>
2022-08-21 07:52:44 +0530

    Rolling the dice
    Added Event Listener to the Roll Dice button

--- Starter Code/complete-javascript-course/07-Pig-Game/starter/script.js
+++ Starter Code/complete-javascript-course/07-Pig-Game/starter/script.js
@@ -4,6 +4,11 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
 // Setting the score to 0 
 score0El.textContent = 0;
@@ -11,3 +16,25 @@ score1El.textContent = 0;
 
 // Disappearing the class by including the 'hidden' class 
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function (){
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. Check for rolled 1: if true, switch to next player
+    if(dice !== 1){
+        // Add dice to the current score
+        currentScore += dice;
+        current0El.textContent = currentScore;
+    } else {
+        // Switch to next player
+    }
+});
