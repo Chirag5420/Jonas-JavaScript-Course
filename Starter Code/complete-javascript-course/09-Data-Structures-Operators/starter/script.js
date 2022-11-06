5f74d4f68c1b668584b79fff32dd7f25fa6ba4c1
Chirag Chhajlani <chiragchhajlani3@gmail.com>
2022-11-06 20:22:56 +0530

    Enhanced Object Literals

--- Starter Code/complete-javascript-course/09-Data-Structures-Operators/starter/script.js
+++ Starter Code/complete-javascript-course/09-Data-Structures-Operators/starter/script.js
@@ -4,6 +4,25 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+// Enhanced Object Literals
+
+// Third Enhancement : We can now even compute property names. So for example, now instead of writing 'thu', we used weekdays[3]
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -12,39 +31,31 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  // First Enhancement : ES6 enhanced object literals
+  openingHours,
 
-  order: function(starterIndex, mainIndex) {
+  // Second Enhancement: In ES6, we no longer need to write the keyword 'function', we can exclude the colon and function keyword to get it to work as a function 
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+  orderDelivery({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
     console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
   },
 
-  orderPasta: function(ing1, ing2, ing3){
+  orderPasta(ing1, ing2, ing3){
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
   },
 
-  orderPizza: function(mainIngredient, ...otherIngredients){
+  orderPizza(mainIngredient, ...otherIngredients){
     console.log(mainIngredient);
     console.log(otherIngredients);
   }
 };
 
+console.log(restaurant);
+/*
+//////////////////////////////////
 // Looping Arrays: The for-of loop
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
@@ -59,6 +70,8 @@ for(const [i, el] of menu.entries()) {
 }
 
 console.log([...menu.entries()]);
+*/
+
 /*
 ///////////////////////////////
 // Logical Assignment Operators 
