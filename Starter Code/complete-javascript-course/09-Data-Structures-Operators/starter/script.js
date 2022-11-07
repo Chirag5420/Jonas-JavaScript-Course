40c4bf6a5f9c8271a11d69d6d7225a22d064e0fc
Chirag Chhajlani <chiragchhajlani3@gmail.com>
2022-11-07 06:16:16 +0530

    Optional Chaining (?)

--- Starter Code/complete-javascript-course/09-Data-Structures-Operators/starter/script.js
+++ Starter Code/complete-javascript-course/09-Data-Structures-Operators/starter/script.js
@@ -53,7 +53,32 @@ const restaurant = {
   }
 };
 
-console.log(restaurant);
+//console.log(restaurant);
+
+// Optional Chaining (?.)
+if(restaurant.openingHours && restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+
+// WITH optional chaining
+console.log(restaurant.openingHours?.mon?.open);
+// So here's how optional chaining works. So only if the property that is before the ? exists, so if monday exists then the open property will be read from there. But if not, immediately undefined will be returned
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for(const day of days){
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+const users = [
+  {name: 'Jonas', email: 'hello@jonas.io'}
+];
+console.log(users[0]?.name ?? 'User array empty');
+
 /*
 //////////////////////////////////
 // Looping Arrays: The for-of loop
