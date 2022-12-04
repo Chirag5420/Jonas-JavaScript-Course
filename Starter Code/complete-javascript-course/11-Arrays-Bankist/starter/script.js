cc9c663648c284bdc54501d41c9443e630d0c97e
Chirag Chhajlani <chiragchhajlani3@gmail.com>
2022-12-04 12:33:25 +0530

    Computing Usernames

--- Starter Code/complete-javascript-course/11-Arrays-Bankist/starter/script.js
+++ Starter Code/complete-javascript-course/11-Arrays-Bankist/starter/script.js
@@ -83,6 +83,22 @@ displayMovements(account1.movements);
 
 // console.log(containerMovements.innerHTML);
 
+// Computing Usernames
+const user = 'Steven Thomas Williams'; // stw
+
+const createUsernames = function (accs) {
+  accs.forEach(function(acc){
+    acc.username = acc.owner
+    .toLowerCase()
+    .split(' ')
+    .map(name => name.charAt(0))
+    .join('');
+  })
+}
+
+createUsernames(accounts);
+console.log(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
