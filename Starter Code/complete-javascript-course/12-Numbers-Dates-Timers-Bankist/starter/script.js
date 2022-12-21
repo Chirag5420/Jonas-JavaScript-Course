de7f5232c076510945529b4afddd6152dbfcde6d
Chirag Chhajlani <chiragchhajlani3@gmail.com>
2022-12-22 00:09:31 +0530

    Implementing a Countdown Timer

--- Starter Code/complete-javascript-course/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ Starter Code/complete-javascript-course/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -182,14 +182,44 @@ const updateUI = function (acc) {
   calcDisplaySummary(acc);
 };
 
+// Implementing a Countdown Timer
+
+const startLogOutTimer = function(){
+  const tick = function(){
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+
+    // In each call, print the remaining time to the UI
+    labelTimer.textContent = `${min}:${sec}`;
+
+    // When 0 seconds, stop timer and log out user
+    if(time === 0){
+      clearInterval(timer);
+      labelWelcome.textContent = 'Log in to get started';
+      containerApp.style.opacity = 0;
+    }
+
+    // Decrease 1s
+    time--;
+  };
+
+  // Set time to 5 minutes
+  let time = 120;
+
+  // Call the timer every second
+  tick();
+  const timer = setInterval(tick, 1000);
+
+  return timer; 
+}
 ///////////////////////////////////////
 // Event handlers
-let currentAccount;
+let currentAccount, timer;
 
-// FAKE ALWAYS LOGGED IN 
-currentAccount = account1;
-updateUI(currentAccount);
-containerApp.style.opacity = 100;
+// // FAKE ALWAYS LOGGED IN 
+// currentAccount = account1;
+// updateUI(currentAccount);
+// containerApp.style.opacity = 100;
 
 // Experimenting API
 
@@ -235,6 +265,11 @@ btnLogin.addEventListener('click', function (e) {
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
 
+    // Timer
+    if(timer) clearInterval(timer);
+
+    timer = startLogOutTimer();
+
     // Update UI
     updateUI(currentAccount);
   }
@@ -264,6 +299,10 @@ btnTransfer.addEventListener('click', function (e) {
 
     // Update UI
     updateUI(currentAccount);
+
+    // Reset timer
+    clearInterval(timer);
+    timer = startLogOutTimer();
   }
 });
 
@@ -282,6 +321,10 @@ btnLoan.addEventListener('click', function (e) {
 
       // Update UI
       updateUI(currentAccount);
+
+      // Reset timer
+      clearInterval(timer);
+      timer = startLogOutTimer();
     }, 2500)
   }
   inputLoanAmount.value = '';
@@ -543,7 +586,7 @@ console.log('Germany: ', new Intl.NumberFormat('de-DE', options).format(num));
 console.log('Syria: ', new Intl.NumberFormat('ar-SY', options).format(num));
 console.log(navigator.language, new Intl.NumberFormat(navigator.language, options).format(num));
 */
-
+/*
 // Timers: setTimeout and setInterval
 
 // setTimeout
@@ -565,3 +608,4 @@ setInterval(function(){
   }
   console.log(new Intl.DateTimeFormat('en-US', options).format(now));
 }, 1000)
+*/
