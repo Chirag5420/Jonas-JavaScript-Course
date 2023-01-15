4c0f1cf0a9ee708bca7b7d4514af3ef0b9e31a98
Chirag Chhajlani <chiragchhajlani3@gmail.com>
2023-01-15 19:48:59 +0530

    Inheritance Between Classes: Constructor Functions

--- Starter Code/complete-javascript-course/14-OOP/starter/script.js
+++ Starter Code/complete-javascript-course/14-OOP/starter/script.js
@@ -294,3 +294,31 @@ sarah.init('Sarah', 1979);
 sarah.calcAge();
 
 console.log(sarah);
+
+// Inheritance Between "Classes": Constructor Functions 
+const Student = function(firstName, birthYear, course){
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+// Linking prototypes
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function(){
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+}
+
+const mike = new Student('Mike', 2020, 'Computer Science');
+console.log(mike);
+mike.introduce();
+mike.calcAge();
+
+console.log(mike.__proto__);
+console.log(mike.__proto__.__proto__);
+
+console.log(mike instanceof Student); // true
+console.log(mike instanceof Person); // true
+console.log(mike instanceof Object); // true
+
+Student.prototype.constructor = Student;
+console.dir(Student.prototype.constructor);
