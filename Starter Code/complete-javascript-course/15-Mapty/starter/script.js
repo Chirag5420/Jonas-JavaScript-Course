ca81250b778b70bd2ed7db951418b9ce3a4812dd
Chirag Chhajlani <chiragchhajlani3@gmail.com>
2023-01-22 13:18:07 +0530

    Displaying a Map Marker

--- Starter Code/complete-javascript-course/15-Mapty/starter/script.js
+++ Starter Code/complete-javascript-course/15-Mapty/starter/script.js
@@ -26,14 +26,30 @@ if(navigator.geolocation){
 
         // The second parameter (13) is the zoom level
         const map = L.map('map').setView(coords, 13);
+        // console.log(map); 
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        L.marker(coords).addTo(map)
-            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        // Displaying a Map Marker
+        // The on() method is similar to the addEventListener method and this method had been declared by the Leaflet library 
+        map.on('click', function(mapEvent){
+            console.log(mapEvent);
+
+            const {lat, lng} = mapEvent.latlng;
+
+            L.marker([lat, lng]).addTo(map)
+            .bindPopup(L.popup({
+                maxWidth: 250,
+                minWidth: 200,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup'
+            }))
+            .setPopupContent('Workout')
             .openPopup();
+        });
     }, function(){
         alert('Could not get your position')
     });
